769f87caa0e2d9c48b10cce056c56919408c1d4d
Walter Mattingly <waltdakind@gmail.com>
2016-03-01 10:45:34 -0500

    timer works past round1-10, needs counters and button logic

--- timer.js
+++ timer.js
@@ -1,28 +1,49 @@
 $( document ).ready(function() {
     answersOntoButtons(i);
+    listenToButtons();
+   
+
     
 });
+//VARIABLES
+
 //questions and answers
 // all the stuff for populating the other stuff
 //===============================================
 var i = 0;
+var round = 1;
 var trivia = [ 
-{"question": "What was Harry Houdini's real name?", "realAnswer": "Erich Weiss", "fakeAnswer1": "Robert Houdin", "fakeAnswer2":"Neville Maskylne", "fakeAnswer3":"Magic Mike"},
-{"question": "Rabbits engage in coprophagy, what is that?", "realAnswer": "Eating their own droppings", "fakeAnswer1": "Mating in large groups", "fakeAnswer2":"Mating multiple times ","fakeAnswer3": "Eating grasses and vegetables"},
-{"question": "Jerry Garcia was missing what finger?", "realAnswer": "Right middle finger", "fakeAnswer1":"Left index finger", "fakeAnswer2":"Right pinkie finger","fakeAnswer3": "Left middle finger"},
-{"question": "Where is New Belgium Brewing Company located?", "realAnswer": "Fort Collins, Colorado", "fakeAnswer1": "New Belgium, Brussels", "fakeAnswer2":"Golden, CO","fakeAnswer3": "Highgate, VT"},
-{"question": "What country of Africa has largest population?", "realAnswer": "Nigeria", "fakeAnswer1": "Kenya", "fakeAnswer2":"Egypt","fakeAnswer3":"Libya"},
-{"question": "Who said -- Do or do not there is no try?", "realAnswer": "Yoda", "fakeAnswer1": "Yogi Berra", "fakeAnswer2":"Robert Schuller","fakeAnswer3": "Matlock"},
-{"question": "In the film, Thelma and Louise, who co-starred with Geena Davis?", "realAnswer": "Susan Sarandon", "fakeAnswer1": "Bea Arthur", "fakeAnswer2":"Betty White","fakeAnswer3": "Meryl Streep"},
-{"question": "If you mixed rum, simple syrup, lime juice and selter with some mint leaves you'd have what?", "realAnswer": "A mojito", "fakeAnswer1": "A Cuba Libre", "fakeAnswer2":"A Manhattan","fakeAnswer3": "A Cosmopolitan"},
-{"question": "In darts getting three bull's eyes in a turn is called what?", "realAnswer": "A hat trick", "fakeAnswer1": "Darn lucky", "fakeAnswer2":"Three in a bed","fakeAnswer3": "A Low Ton"},
-{"question": "What was the Beatles' first single in 1962?", "realAnswer": "Love Me Do", "fakeAnswer1": "Help!", "fakeAnswer2":"I Saw Her Standing There","fakeAnswer3": "I Am the Walrus"},
-{"question": "What country was Vincent VanGogh from?", "realAnswer": "The Netherlands", "fakeAnswer1": "France","fakeAnswer2": "Germany","fakeAnswer3": "Austria"},
-{"question": "What is the most widely eaten fish in the world?", "realAnswer": "Herring", "fakeAnswer1": "Trout","fakeAnswer2": "Salmon","fakeAnswer3": "Tilapia"}];
+{"question": "What was Harry Houdini's real name?", "realAnswer": "Erich Weiss", "fakeAnswer1": "Robert Houdin", "fakeAnswer2":"Neville Maskylne", "fakeAnswer3":"Magic Mike", "answeredCorrectly":false},
+{"question": "Rabbits engage in coprophagy, what is that?", "realAnswer": "Eating their own droppings", "fakeAnswer1": "Mating in large groups", "fakeAnswer2":"Mating multiple times ","fakeAnswer3": "Eating grasses and vegetables", "answeredCorrectly":false},
+{"question": "Jerry Garcia was missing what finger?", "realAnswer": "Right middle finger", "fakeAnswer1":"Left index finger", "fakeAnswer2":"Right pinkie finger","fakeAnswer3": "Left middle finger", "answeredCorrectly":false},
+{"question": "Where is New Belgium Brewing Company located?", "realAnswer": "Fort Collins, Colorado", "fakeAnswer1": "New Belgium, Brussels", "fakeAnswer2":"Golden, CO","fakeAnswer3": "Highgate, VT", "answeredCorrectly":false},
+{"question": "What country of Africa has largest population?", "realAnswer": "Nigeria", "fakeAnswer1": "Kenya", "fakeAnswer2":"Egypt","fakeAnswer3":"Libya", "answeredCorrectly":false},
+{"question": "Who said -- Do or do not there is no try?", "realAnswer": "Yoda", "fakeAnswer1": "Yogi Berra", "fakeAnswer2":"Robert Schuller","fakeAnswer3": "Matlock", "answeredCorrectly":false},
+{"question": "In the film, Thelma and Louise, who co-starred with Geena Davis?", "realAnswer": "Susan Sarandon", "fakeAnswer1": "Bea Arthur", "fakeAnswer2":"Betty White","fakeAnswer3": "Meryl Streep", "answeredCorrectly":false},
+{"question": "If you mixed rum, simple syrup, lime juice and selter with some mint leaves you'd have what?", "realAnswer": "A mojito", "fakeAnswer1": "A Cuba Libre", "fakeAnswer2":"A Manhattan","fakeAnswer3": "A Cosmopolitan", "answeredCorrectly":false},
+{"question": "In darts getting three bull's eyes in a turn is called what?", "realAnswer": "A hat trick", "fakeAnswer1": "Darn lucky", "fakeAnswer2":"Three in a bed","fakeAnswer3": "A Low Ton", "answeredCorrectly":false},
+{"question": "What was the Beatles' first single in 1962?", "realAnswer": "Love Me Do", "fakeAnswer1": "Help!", "fakeAnswer2":"I Saw Her Standing There","fakeAnswer3": "I Am the Walrus", "answeredCorrectly":false},
+{"question": "What country was Vincent VanGogh from?", "realAnswer": "The Netherlands", "fakeAnswer1": "France","fakeAnswer2": "Germany","fakeAnswer3": "Austria", "answeredCorrectly":false},
+{"question": "What is the most widely eaten fish in the world?", "realAnswer": "Herring", "fakeAnswer1": "Trout","fakeAnswer2": "Salmon","fakeAnswer3": "Tilapia", "answeredCorrectly":false}];
 //=========================================================
 // this is the syntax that works! 
 //  var questionForDisplay = trivia[i].question;
 //==========================================================
+
+var listenToButtons = function() {
+    $('#butt1').on('click', function() {
+     alert('butt1!!!!');
+    });
+         $('#butt2').on('click', function() {
+     alert('butt2!!!!');
+    });
+                 $('#butt3').on('click', function() {
+     alert('butt3!!!!');
+    });
+                         $('#butt4').on('click', function() {
+     alert('butt4!!!!');
+    });
+}
 
 
 var answersOntoButtons = function(num) {
@@ -68,6 +89,16 @@ switch(randomSpot) {
         break;
 }  //end switch
 
+//evaluate answer
+
+
+
+
+//update game counters
+
+
+
+
 //==========================================================
 
 // Declaring class "Timer"
@@ -110,6 +141,7 @@ var Timer = function() {
     {            
         thisObject.Enable = new Boolean(false);
         clearInterval(thisObject.timerId);
+        round++;
     };
 
 };
@@ -118,12 +150,14 @@ var index = 30;
 var obj = new Timer();
 obj.Interval = 1000;
 obj.Tick = timer_tick;
+
 obj.Start();
 //==================
 function timer_tick()
 {
     index  = index - 1;
     document.getElementById("timerOut").innerHTML = "You have "+index+" seconds remaining to select your answer";
+
     //$("#butt1").text("Hello world!");
     if (index <= 0) {
         obj.Stop();
@@ -131,7 +165,7 @@ function timer_tick()
         i++;
         index = 30;
         answersOntoButtons(i);  
-
+        listenToButtons;
         obj.Start();
         
     }
